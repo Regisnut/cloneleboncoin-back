80a35751037d387d9066abbca9690a0322a3d0bc
Régis NUTTIN <regisnut@hotmail.com>
2020-04-23 21:13:00 +0200

    master commit

--- index.js
+++ index.js
@@ -21,15 +21,20 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/leboncoin', {
 require('./models/user');
 require('./models/offer');
 
-// ** ROUTES **
+// ** ROUTES USER OFFER PAYMENT**
+
 const userRoutes = require('./routes/user');
 app.use(userRoutes);
+
 
 const offersRoutes = require('./routes/offer');
 app.use(offersRoutes);
 
+
 const paymentRoutes = require("./routes/payment")
 app.use(paymentRoutes);
+
+
 
 app.all('*', (req, res) => {
 	res.status(404).send({ message: 'Page not found' });
