cb5563a96f7747fd9f2954a106835c14cd4ecb98
Régis NUTTIN <regisnut@hotmail.com>
2020-04-26 01:11:51 +0200

    fix middleware

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -3,6 +3,7 @@ const express = require('express');
 const User = require('../models/user');
 
 const isAuthenticate = async (req, res, next) => {
+	console.log('req.headers.authorization', req.headers.authorization);
 	// Si j'ai un bien entré un token dans le post :
 	const auth = req.headers.authorization;
 	try {
@@ -10,12 +11,13 @@ const isAuthenticate = async (req, res, next) => {
 			const user = await User.findOne({
 				token: auth.replace('Bearer ', '')
 			}); // Va chercher le User avec cet exact token entré sans le bearer
-
+			console.log('user', user);
 			if (!user) {
 				// Si je n'ai pas trouvé de user correspondant
 				res.status(401).json({ message: 'Invalid Token' });
 			} else {
 				req.user = user; // la requête req va avoir une clé supplémentaire appelée "user" qui aura toutes les infos de user ()
+				console.log('req.user', req.user);
 				next();
 			}
 		} else {
@@ -23,7 +25,7 @@ const isAuthenticate = async (req, res, next) => {
 			res.status(401).json({ message: 'Missing Authorization Header' });
 		}
 	} catch (error) {
-		res.json({ message: error.message });
+		res.json({ message: error.message, error: 'Unauthorized' });
 	}
 };
 
