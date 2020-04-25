394e178174baaed4bb936d383d70f211b149d57d
Régis NUTTIN <regisnut@hotmail.com>
2020-04-25 12:02:20 +0200

    cloudinary

--- models/offer.js
+++ models/offer.js
@@ -12,7 +12,7 @@ const Offer = mongoose.model('Offer', {
 		maxlength: 500
 	},
 	price: { type: Number, min: 0, max: 100000 },
-	picture: { type: Array },
+	picture: { type: Object },
 	created: {
 		type: Date,
 		default: Date.now()
